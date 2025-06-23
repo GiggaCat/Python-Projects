5817287ae9fd6a35470f7a538a1ea0091836eaec
Vansh Arora <146607177+GiggaCat@users.noreply.github.com>
2025-06-23 22:06:55 +0530

    Add files via upload

---
+++ DESKTOP-ASSISTANT/music_library.py
@@ -0,0 +1,5 @@
+music = {
+    "rasiya":"https://youtu.be/F_oNAi5DcEU?si=RRIQn_V6Tb1PNstA",
+    "ishq de fanniyaar":"https://youtu.be/LySSIpmJQxg?si=8hIeLK0bkC6eK-TA",
+    "aaj bhi": "https://youtu.be/K0ibsAQTyRQ?si=MBNyyKhAiI3OF8OM"
+}
